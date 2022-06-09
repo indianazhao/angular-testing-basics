943502fdc54111fc2b4a380be4032474f3c0e89c
indianazhao <indiana.zhao@gmail.com>
2022-06-09 11:18:32 +0800

    Hirez.io Angular Testing Basics EP21-EP22: 使用 jasmine-auto-spies。

--- src/app/front/front.component.spec.ts
+++ src/app/front/front.component.spec.ts
@@ -2,11 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FrontComponent } from './front.component';
 import { FrontService } from './front.service';
+import { createSpyFromClass, Spy } from 'jasmine-auto-spies';
 
 describe('FrontComponent', () => {
   let componentUnderTest: FrontComponent;
   let actualValue: any;
-  let frontServiceSpy: jasmine.SpyObj<FrontService>;
+  let frontServiceSpy: Spy<FrontService>;
 
   Given(() => {
     TestBed.configureTestingModule({
@@ -14,21 +15,19 @@ describe('FrontComponent', () => {
         FrontComponent,
         {
           provide: FrontService,
-          useValue: jasmine.createSpyObj(
-            'frontServiceSpy',
-            {
-              getFeaturedLlamas: [{name: 'indi', imageFileName: 'fakeImage'}]
-            }
-          )
+          useValue: createSpyFromClass(FrontService)
         }
       ]
     });
 
     componentUnderTest = TestBed.inject(FrontComponent);
-    frontServiceSpy = TestBed.inject(FrontService) as jasmine.SpyObj<FrontService>;
+    frontServiceSpy = TestBed.inject(FrontService) as Spy<FrontService>;
   });
 
   describe('INIT', () => {
+    Given(() => {
+      frontServiceSpy.getFeaturedLlamas.and.returnValue([{ name: 'indi', imageFileName: 'fakeImage' }]);
+    });
     When(() => {
       actualValue = componentUnderTest.ngOnInit();
     });
